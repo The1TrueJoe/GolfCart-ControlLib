fffc1f2462abfbcc2fb2ff22ef5d7e3bbd881678
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2022-05-15 19:13:42 -0400

    Misc fixes (#7)
    * detach Control Library
    
    * Create README.md
    
    * Rename
    
    * Fix imports
    
    * Blink turn signals
    
    * Defaults and added functions
    
    * Alter logging
    
    * Rename manual string
    
    * Send init string to adapter
    
    * Config example
    
    * Deobjectify

--- ControlLib/src/modules/accessory_ctrl.py
+++ ControlLib/src/modules/accessory_ctrl.py
@@ -36,221 +36,170 @@ class Accessory_Controller:
         else:
             self.logger = None
 
-        # Components
-        self.right_signal = self.Right_Signal(can_address=self.can_address, logger=self.logger)
-        self.left_signal = self.Left_Signal(can_address=self.can_address, logger=self.logger)
-        self.head_light = self.Head_Lights(can_address=self.can_address, logger=self.logger)
-        self.tail_light = self.Tail_Lights(can_address=self.can_address, logger=self.logger)
-        self.horn = self.Horn(can_address=self.can_address, logger=self.logger)
-        self.rear_buzzer = self.Rear_Buzzer(can_address=self.can_address, logger=self.logger)
-
 
     # ----------------------------
     # Right Turn Signal
     # ----------------------------
 
-    class Right_Signal:
+    # Blink the Right Signal
+    def right_signal_blink(self):
+        if not self.logger == None:
+            self.logger.debug(f"Blink Right Signal")
+        return f"({self.can_address}) 11 1 0 0 0 0 0 0"
 
-        # Constructor
-        def __init__(self, can_address, logger = None):
-            self.can_address = can_address
-            self.logger = logger
+    # Turn on the Right Signal
+    def right_signal_on(self):
+        if not self.logger == None:
+            self.logger.debug("Turn On Right Signal")
+        return f"({self.can_address}) 10 1 1 0 0 0 0 0"
 
-        # Blink the Right Signal
-        def blink(self):
-            if not self.logger == None:
-                self.logger.debug(f"Blink Right Signal")
-            return f"({self.can_address}) 11 1 0 0 0 0 0 0"
+    # Turn off the Right Signal
+    def right_signal_off(self):
+        if not self.logger == None:
+            self.logger.debug("Turn Off Right Signal")
+        return f"({self.can_address}) 10 1 2 0 0 0 0 0"
 
-        # Turn on the Right Signal
-        def on(self):
-            if not self.logger == None:
-                self.logger.debug("Turn On Right Signal")
-            return f"({self.can_address}) 10 1 1 0 0 0 0 0"
-
-        # Turn off the Right Signal
-        def off(self):
-            if not self.logger == None:
-                self.logger.debug("Turn Off Right Signal")
-            return f"({self.can_address}) 10 1 2 0 0 0 0 0"
-
-        # Request the Right Signal Setting
-        def get(self):
-            if not self.logger == None:
-                self.logger.debug("Get Right Signal Setting")
-            return f"({self.can_address}) 12 1 0 0 0 0 0 0"
+    # Request the Right Signal Setting
+    def right_signal_get(self):
+        if not self.logger == None:
+            self.logger.debug("Get Right Signal Setting")
+        return f"({self.can_address}) 12 1 0 0 0 0 0 0"
 
 
     # ----------------------------
     # Left Turn Signal
     # ----------------------------
-    
-    class Left_Signal:
 
-        # Constructor
-        def __init__(self, can_address, logger = None):
-            self.can_address = can_address
-            self.logger = logger
+    # Blink the Left Signal
+    def left_signal_blink(self):
+        if not self.logger == None:
+            self.logger.debug(f"Blink Left Signal")
+        return f"({self.can_address}) 11 2 0 0 0 0 0 0"
 
-        # Blink the Left Signal
-        def blink(self):
-            if not self.logger == None:
-                self.logger.debug(f"Blink Left Signal")
-            return f"({self.can_address}) 11 2 0 0 0 0 0 0"
+    # Turn on the Left Signal
+    def left_signal_on(self):
+        if not self.logger == None:
+            self.logger.debug("Turn On Left Signal")
+        return f"({self.can_address}) 10 2 1 0 0 0 0 0"
 
-        # Turn on the Left Signal
-        def on(self):
-            if not self.logger == None:
-                self.logger.debug("Turn On Left Signal")
-            return f"({self.can_address}) 10 2 1 0 0 0 0 0"
+    # Turn off the Left Signal
+    def left_signal_off(self):
+        if not self.logger == None:
+            self.logger.debug("Turn Off Left Signal")
+        return f"({self.can_address}) 10 2 2 0 0 0 0 0"
 
-        # Turn off the Left Signal
-        def off(self):
-            if not self.logger == None:
-                self.logger.debug("Turn Off Left Signal")
-            return f"({self.can_address}) 10 2 2 0 0 0 0 0"
-
-        # Request the Left Signal Setting
-        def get(self):
-            if not self.logger == None:
-                self.logger.debug("Get Left Signal Setting")
-            return f"({self.can_address}) 12 2 0 0 0 0 0 0"
+    # Request the Left Signal Setting
+    def left_signal_get(self):
+        if not self.logger == None:
+            self.logger.debug("Get Left Signal Setting")
+        return f"({self.can_address}) 12 2 0 0 0 0 0 0"
 
 
     # ----------------------------
     # Head Lights
     # ----------------------------
-    
-    class Head_Lights:
 
-        # Constructor
-        def __init__(self, can_address, logger = None):
-            self.can_address = can_address
-            self.logger = logger
+    # Blink the Head Lights
+    def head_lights_blink(self):
+        if not self.logger == None:
+            self.logger.debug(f"Blink Head Lights")
+        return f"({self.can_address}) 11 3 0 0 0 0 0 0"
 
-        # Blink the Head Lights
-        def blink(self):
-            if not self.logger == None:
-                self.logger.debug(f"Blink Head Lights")
-            return f"({self.can_address}) 11 3 0 0 0 0 0 0"
+    # Turn on the Head Lights
+    def head_lights_on(self):
+        if not self.logger == None:
+            self.logger.debug("Turn On Head Lights")
+        return f"({self.can_address}) 10 3 1 0 0 0 0 0"
 
-        # Turn on the Head Lights
-        def on(self):
-            if not self.logger == None:
-                self.logger.debug("Turn On Head Lights")
-            return f"({self.can_address}) 10 3 1 0 0 0 0 0"
+    # Turn off the Head Lights
+    def head_lights_off(self):
+        if not self.logger == None:
+            self.logger.debug("Turn Off Head Lights")
+        return f"({self.can_address}) 10 3 2 0 0 0 0 0"
 
-        # Turn off the Head Lights
-        def off(self):
-            if not self.logger == None:
-                self.logger.debug("Turn Off Head Lights")
-            return f"({self.can_address}) 10 3 2 0 0 0 0 0"
-
-        # Request the Head Light Setting
-        def get(self):
-            if not self.logger == None:
-                self.logger.debug("Get Head Light Setting")
-            return f"({self.can_address}) 12 3 0 0 0 0 0 0"
+    # Request the Head Light Setting
+    def head_lights_get(self):
+        if not self.logger == None:
+            self.logger.debug("Get Head Light Setting")
+        return f"({self.can_address}) 12 3 0 0 0 0 0 0"
 
 
     # ----------------------------
     # Tail Lights
     # ----------------------------
-    
-    class Tail_Lights:
 
-        # Constructor
-        def __init__(self, can_address, logger = None):
-            self.can_address = can_address
-            self.logger = logger
+    # Blink the Tail Lights
+    def tail_lights_blink(self):
+        if not self.logger == None:
+            self.logger.debug(f"Blink Tail Lights")
+        return f"({self.can_address}) 11 4 0 0 0 0 0 0"
 
-        # Blink the Tail Lights
-        def blink(self):
-            if not self.logger == None:
-                self.logger.debug(f"Blink Tail Lights")
-            return f"({self.can_address}) 11 4 0 0 0 0 0 0"
+    # Turn on the Tail Lights
+    def tail_lights_on(self):
+        if not self.logger == None:
+            self.logger.debug("Turn On Tail Lights")
+        return f"({self.can_address}) 10 4 1 0 0 0 0 0"
 
-        # Turn on the Tail Lights
-        def on(self):
-            if not self.logger == None:
-                self.logger.debug("Turn On Tail Lights")
-            return f"({self.can_address}) 10 4 1 0 0 0 0 0"
+    # Turn off the Tail Lights
+    def tail_lights_off(self):
+        if not self.logger == None:
+            self.logger.debug("Turn Off Tail Lights")
+        return f"({self.can_address}) 10 4 2 0 0 0 0 0"
 
-        # Turn off the Tail Lights
-        def off(self):
-            if not self.logger == None:
-                self.logger.debug("Turn Off Tail Lights")
-            return f"({self.can_address}) 10 4 2 0 0 0 0 0"
-
-        # Request the Tail Light Setting
-        def get(self):
-            if not self.logger == None:
-                self.logger.debug("Get Tail Light Setting")
-            return f"({self.can_address}) 12 4 0 0 0 0 0 0"
+    # Request the Tail Light Setting
+    def tail_lights_get(self):
+        if not self.logger == None:
+            self.logger.debug("Get Tail Light Setting")
+        return f"({self.can_address}) 12 4 0 0 0 0 0 0"
 
 
     # ----------------------------
     # Horn
     # ----------------------------
 
-    class Horn:
+    # Honk the Horn
+    def horn_honk(self):
+        if not self.logger == None:
+            self.logger.debug(f"Honk Horn")
+        return f"({self.can_address}) 11 1 5 0 0 0 0 0"
 
-        # Constructor
-        def __init__(self, can_address, logger = None):
-            self.can_address = can_address
-            self.logger = logger
+    # Turn on the horn
+    def horn_on(self):
+        if not self.logger == None:
+            self.logger.debug("Turn on Horn")
+        return f"({self.can_address}) 10 5 1 0 0 0 0 0"
 
-        # Honk the Horn
-        def honk(self):
-            if not self.logger == None:
-                self.logger.debug(f"Honk Horn")
-            return f"({self.can_address}) 11 1 5 0 0 0 0 0"
+    # Turn off the horn
+    def horn_off(self):
+        if not self.logger == None:
+            self.logger.debug("Turn Off Horn")
+        return f"({self.can_address}) 10 5 2 0 0 0 0 0"
 
-        # Turn on the horn
-        def on(self):
-            if not self.logger == None:
-                self.logger.debug("Turn on Horn")
-            return f"({self.can_address}) 10 5 1 0 0 0 0 0"
-
-        # Turn off the horn
-        def off(self):
-            if not self.logger == None:
-                self.logger.debug("Turn Off Horn")
-            return f"({self.can_address}) 10 5 2 0 0 0 0 0"
-
-        # Request the horn status
-        def get(self):
-            if not self.logger == None:
-                self.logger.debug("Get Horn Status")
-            return f"({self.can_address}) 12 5 0 0 0 0 0 0"
+    # Request the horn status
+    def horn_get(self):
+        if not self.logger == None:
+            self.logger.debug("Get Horn Status")
+        return f"({self.can_address}) 12 5 0 0 0 0 0 0"
 
 
     # ----------------------------
     # Rear Buzzer
     # ----------------------------
 
-    class Rear_Buzzer:
+    # Turn on the Rear Buzzer
+    def rear_buzz_on(self):
+        if not self.logger == None:
+            self.logger.debug("Turning on Rear Buzzer")
+        return f"({self.can_address}) 10 6 1 0 0 0 0 0"
 
-        # Constructor
-        def __init__(self, can_address, logger = None):
-            self.can_address = can_address
-            self.logger = logger
+    # Turn off the Rear Buzzer
+    def ear_buzz_off(self):
+        if not self.logger == None:
+            self.logger.debug("Turning Off Rear Buzzer")
+        return f"({self.can_address}) 10 6 2 0 0 0 0 0"
 
-        # Turn on the Rear Buzzer
-        def on(self):
-            if not self.logger == None:
-                self.logger.debug("Turning on Rear Buzzer")
-            return f"({self.can_address}) 10 6 1 0 0 0 0 0"
-
-        # Turn off the Rear Buzzer
-        def off(self):
-            if not self.logger == None:
-                self.logger.debug("Turning Off Rear Buzzer")
-            return f"({self.can_address}) 10 6 2 0 0 0 0 0"
-
-        # Request the Rear Buzzer status
-        def get(self):
-            if not self.logger == None:
-                self.logger.debug("Get Rear Buzzer Status")
-            return f"({self.can_address}) 12 6 0 0 0 0 0 0"
-
+    # Request the Rear Buzzer status
+    def ear_buzz_get(self):
+        if not self.logger == None:
+            self.logger.debug("Get Rear Buzzer Status")
+        return f"({self.can_address}) 12 6 0 0 0 0 0 0"
