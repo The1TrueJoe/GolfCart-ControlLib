6896aa5cd0e692ccf26777b74a80dffbbf69986b
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2022-05-21 05:36:13 -0400

    Removing logging from message objects (#10)
    * detach Control Library
    
    * Create README.md
    
    * Rename
    
    * Fix imports
    
    * Blink turn signals
    
    * Defaults and added functions
    
    * Alter logging
    
    * Rename manual string
    
    * Send init string to adapter
    
    * Config example
    
    * Deobjectify
    
    * Fix messages
    
    * Update messages
    
    * Update messages
    
    * Remove args
    
    * Remove message
    
    * Removing logging from hardware abstractions

--- ControlLib/src/modules/accessory_ctrl.py
+++ ControlLib/src/modules/accessory_ctrl.py
@@ -1,5 +1,3 @@
-import logging
-
 # Drive Computer Core Library
 # Accessory Controller Module
 #
@@ -22,38 +20,24 @@ class Accessory_Controller:
         # CAN Address
         self.can_address = 2
 
-        # Setup the message logging
-        self.logger = logging.getLogger("accessory_controller")
-        file_handler = logging.FileHandler("logs/accessory_ctrl.log")
-        file_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-        self.logger.addHandler(file_handler)
-
-        # Init Message
-        self.logger(f"Initializing Accessory Controller at address: {self.can_address}")
-
-
     # ----------------------------
     # Right Turn Signal
     # ----------------------------
 
     # Blink the Right Signal
     def right_signal_blink(self):
-        self.logger.debug(f"Blink Right Signal")
         return f"({self.can_address}) 11 1 0 0 0 0 0 0"
 
     # Turn on the Right Signal
     def right_signal_on(self):
-        self.logger.debug("Turn On Right Signal")
         return f"({self.can_address}) 10 1 1 0 0 0 0 0"
 
     # Turn off the Right Signal
     def right_signal_off(self):
-        self.logger.debug("Turn Off Right Signal")
         return f"({self.can_address}) 10 1 2 0 0 0 0 0"
 
     # Request the Right Signal Setting
     def right_signal_get(self):
-        self.logger.debug("Get Right Signal Setting")
         return f"({self.can_address}) 12 1 0 0 0 0 0 0"
 
 
@@ -63,22 +47,18 @@ class Accessory_Controller:
 
     # Blink the Left Signal
     def left_signal_blink(self):
-        self.logger.debug(f"Blink Left Signal")
         return f"({self.can_address}) 11 2 0 0 0 0 0 0"
 
     # Turn on the Left Signal
     def left_signal_on(self):
-        self.logger.debug("Turn On Left Signal")
         return f"({self.can_address}) 10 2 1 0 0 0 0 0"
 
     # Turn off the Left Signal
     def left_signal_off(self):
-        self.logger.debug("Turn Off Left Signal")
         return f"({self.can_address}) 10 2 2 0 0 0 0 0"
 
     # Request the Left Signal Setting
     def left_signal_get(self):
-        self.logger.debug("Get Left Signal Setting")
         return f"({self.can_address}) 12 2 0 0 0 0 0 0"
 
 
@@ -88,22 +68,18 @@ class Accessory_Controller:
 
     # Blink the Head Lights
     def head_lights_blink(self):
-        self.logger.debug(f"Blink Head Lights")
         return f"({self.can_address}) 11 3 0 0 0 0 0 0"
 
     # Turn on the Head Lights
     def head_lights_on(self):
-        self.logger.debug("Turn On Head Lights")
         return f"({self.can_address}) 10 3 1 0 0 0 0 0"
 
     # Turn off the Head Lights
     def head_lights_off(self):
-        self.logger.debug("Turn Off Head Lights")
         return f"({self.can_address}) 10 3 2 0 0 0 0 0"
 
     # Request the Head Light Setting
     def head_lights_get(self):
-        self.logger.debug("Get Head Light Setting")
         return f"({self.can_address}) 12 3 0 0 0 0 0 0"
 
 
@@ -113,22 +89,18 @@ class Accessory_Controller:
 
     # Blink the Tail Lights
     def tail_lights_blink(self):
-        self.logger.debug(f"Blink Tail Lights")
         return f"({self.can_address}) 11 4 0 0 0 0 0 0"
 
     # Turn on the Tail Lights
     def tail_lights_on(self):
-        self.logger.debug("Turn On Tail Lights")
         return f"({self.can_address}) 10 4 1 0 0 0 0 0"
 
     # Turn off the Tail Lights
     def tail_lights_off(self):
-        self.logger.debug("Turn Off Tail Lights")
         return f"({self.can_address}) 10 4 2 0 0 0 0 0"
 
     # Request the Tail Light Setting
     def tail_lights_get(self):
-        self.logger.debug("Get Tail Light Setting")
         return f"({self.can_address}) 12 4 0 0 0 0 0 0"
 
 
@@ -138,22 +110,18 @@ class Accessory_Controller:
 
     # Honk the Horn
     def horn_honk(self):
-        self.logger.debug(f"Honk Horn")
         return f"({self.can_address}) 11 1 5 0 0 0 0 0"
 
     # Turn on the horn
     def horn_on(self):
-        self.logger.debug("Turn on Horn")
         return f"({self.can_address}) 10 5 1 0 0 0 0 0"
 
     # Turn off the horn
     def horn_off(self):
-        self.logger.debug("Turn Off Horn")
         return f"({self.can_address}) 10 5 2 0 0 0 0 0"
 
     # Request the horn status
     def horn_get(self):
-        self.logger.debug("Get Horn Status")
         return f"({self.can_address}) 12 5 0 0 0 0 0 0"
 
 
@@ -163,15 +131,12 @@ class Accessory_Controller:
 
     # Turn on the Rear Buzzer
     def rear_buzz_on(self):
-        self.logger.debug("Turning on Rear Buzzer")
         return f"({self.can_address}) 10 6 1 0 0 0 0 0"
 
     # Turn off the Rear Buzzer
     def ear_buzz_off(self):
-        self.logger.debug("Turning Off Rear Buzzer")
         return f"({self.can_address}) 10 6 2 0 0 0 0 0"
 
     # Request the Rear Buzzer status
     def ear_buzz_get(self):
-        self.logger.debug("Get Rear Buzzer Status")
         return f"({self.can_address}) 12 6 0 0 0 0 0 0"
--- ControlLib/src/modules/direction_ctrl.py
+++ ControlLib/src/modules/direction_ctrl.py
@@ -1,5 +1,4 @@
 import ControlLib.ControlLib.src.can_adapter as can_util
-import logging
 
 # Drive Computer Core Library
 # Direction Controller Module
@@ -16,12 +15,6 @@ class Direction_Controller:
     def __int__(self):
         # CAN Address
         self.can_address = 1
-
-        # Setup the message logging
-        self.logger = logging.getLogger("direction_controller")
-        file_handler = logging.FileHandler("logs/direction_ctrl.log")
-        file_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-        self.logger.addHandler(file_handler)
     
     # ----------------------------
     # Steering Motor Controller
@@ -29,35 +22,28 @@ class Direction_Controller:
 
     # Disable Steering Motor
     def str_disable(self):
-        self.logger.debug("Disabling Steering Motor")
         return f"({self.can_address}) 10 1 10 1 0 0 0 0"
 
     # Enable Steering Motor
     def str_enable(self):
-        self.logger.debug("Enabling Steering Motor")
         return f"({self.can_address}) 10 1 10 2 0 0 0 0"
 
     def str_reqStatus(self):
-        self.logger.debug("Requesting the Steering Motor Status")
         return f"({self.can_address}) 12 1 10 0 0 0 0"
 
     # Run Motor Forwards
     def str_left(self, power = 255):
-        self.logger.debug(f"Running Steering Motor Forwards, Power: {power}")
         return f"({self.can_address}) 10 1 12 1 {power} 0 0 0"
 
     # Run Motor Backwards
     def str_right(self, power = 255):
-        self.logger.debug(f"Running Steering Motor Backwards, Power: {power}")
         return f"({self.can_address}) 10 1 12 2 {power} 0 0 0"
 
     def str_goTo(self, postion, power = 255):
-        self.logger.debug(f"Running to Postion {postion} at {power}")
         return f"({self.can_address}) 11 1 {can_util.sixteentoeight_coarse(postion)} {can_util.sixteentoeight_fine(postion)} {power} 0 0 0"
 
     # Request the steering motor position
     def str_reqPos(self):
-        self.logger.debug("Requesting to Current Steering Motor Position")
         return f"({self.can_address}) 12 1 16 0 0 0 0"
 
     # ----------------------------
@@ -65,7 +51,6 @@ class Direction_Controller:
     # ----------------------------
 
     def wheel_reqPos(self):
-        self.logger.info("Requesing Steering Wheel Change")
         return f"({self.can_address}) 12 1 15 0 0 0 0 0"
 
     # ----------------------------
@@ -74,29 +59,23 @@ class Direction_Controller:
 
     # Disable Brake Motor
     def brk_disable(self):
-        self.logger.debug("Disabling Brake Motor")
         return f"({self.can_address}) 10 2 10 1 0 0 0 0"
 
     # Enable Steering Motor
     def brk_enable(self):
-        self.logger.debug("Enabling Brake Motor")
         return f"({self.can_address}) 10 2 10 2 0 0 0 0"
 
     def brk_reqStatus(self):
-        self.logger.debug("Requesting the Brake Motor Status")
         return f"({self.can_address}) 12 2 10 0 0 0 0"
 
     # Run Motor Forwards
     def brk_pull(self, power = 255):
-        self.logger.debug(f"Running Brake Motor Forwards, Power: {power}")
         return f"({self.can_address}) 10 2 12 1 {power} 0 0 0"
 
     # Run Motor Backwards
     def brk_push(self, power = 255):
-        self.logger.debug(f"Running Brake Motor Backwards, Power: {power}")
         return f"({self.can_address}) 10 2 12 2 {power} 0 0 0"
 
     # Request the steering motor position
     def brk_reqPos(self):
-        self.logger.debug("Requesting to Current Brake Motor Position")
         return f"({self.can_address}) 12 2 16 0 0 0 0"
