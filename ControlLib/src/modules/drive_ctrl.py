c04d2d6b845b7dae8cb1ed391200d88bc77b1d96
The1TrueJoe <jttelaak@hotmail.com>
2022-05-21 05:15:23 -0400

    Remove args

--- ControlLib/src/modules/drive_ctrl.py
+++ ControlLib/src/modules/drive_ctrl.py
@@ -16,44 +16,34 @@ import logging
 
 class Drive_Controller:
 
-    def __int__(self, can_address = 3, log_messages: bool = True):
+    def __int__(self):
         # CAN Address
-        self.can_address = can_address
+        self.can_address = 3
 
-        self.log_messages = log_messages
-        if self.log_messages:
-            # Setup the message logging
-            self.logger = logging.getLogger("drive_controller")
-            file_handler = logging.FileHandler("logs/drive_ctrl.log")
-            file_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-            self.logger.addHandler(file_handler)
-
-        else:
-            self.logger = None
+        # Setup the message logging
+        self.logger = logging.getLogger("drive_controller")
+        file_handler = logging.FileHandler("logs/drive_ctrl.log")
+        file_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+        self.logger.addHandler(file_handler)
 
     def setAccelPos(self, pos):
-        if not self.logger == None:
-            self.logger.info("Setting Accelerator Pot Position")
+        self.logger.info("Setting Accelerator Pot Position")
         return f"({self.can_address}) 10 10 10 {pos} 0 0 0 0"
 
     def increment(self):
-        if not self.logger == None:
-            self.logger.info("Incrementing Accelerator")
+        self.logger.info("Incrementing Accelerator")
         return f"({self.can_address}) 11 10 1 0 0 0 0 0"
 
     def increment(self, count):
-        if not self.logger == None:
-            self.logger.info(f"Incrementing Accelerator by {count}")
+        elf.logger.info(f"Incrementing Accelerator by {count}")
         return f"({self.can_address}) 11 10 1 {count} 0 0 0 0"
 
     def decrement(self):
-        if not self.logger == None:
-            self.logger.info("Decrementing Accelerator")
+        elf.logger.info("Decrementing Accelerator")
         return f"({self.can_address}) 11 10 2 0 0 0 0 0"
 
     def decrement(self, count):
-        if not self.logger == None:
-            self.logger.info(f"Decrementing Accelerator by {count}")
+        self.logger.info(f"Decrementing Accelerator by {count}")
         return f"({self.can_address}) 11 10 2 {count} 0 0 0 0"
 
     def reqAccelPos(self):
@@ -61,40 +51,29 @@ class Drive_Controller:
         return f"({self.can_address}) 12 10 10 0 0 0 0 0"
 
     def enable(self):
-        if not self.logger == None:
-            self.logger.info("Enabling Digital Accelerator")
+        self.logger.info("Enabling Digital Accelerator")
         return f"({self.can_address}) 10 10 15 2 0 0 0 0"
 
     def disable(self):
-        if not self.logger == None:
-            self.logger.info("Disabling Digital Accelerator")
+        self.logger.info("Disabling Digital Accelerator")
         return f"({self.can_address}) 10 10 15 1 0 0 0 0"
 
     def reqEn(self):
-        if not self.logger == None:
-            self.logger.info("Requesing Enable Status")
+        self.logger.info("Requesing Enable Status")
         return f"({self.can_address}) 12 10 15 0 0 0 0 0"
 
     def reqPedalPos(self):
-        if not self.logger == None:
-            self.logger.info("Requesing Accelerator Pedal Pos")
+        self.logger.info("Requesing Accelerator Pedal Pos")
         return f"({self.can_address} 12 10 13 0 0 0 0 0"
 
-    def __init__(self, can_address, logger = None):
-        self.logger = logger
-        self.can_address = can_address
-
     def reverse(self):
-        if not self.logger == None:
-            self.logger.info("Switching to Reverse")
+        self.logger.info("Switching to Reverse")
         return f"({self.can_address}) 10 13 2 0 0 0 0 0"
 
     def forwards(self):
-        if not self.logger == None:
-            self.logger.info("Switching to Forwards")
+        self.logger.info("Switching to Forwards")
         return f"({self.can_address}) 10 13 1 0 0 0 0 0"
 
     def reqDirection(self):
-        if not self.logger == None:
-            self.logger.info("Requesing Direction")
+        self.logger.info("Requesing Direction")
         return f"({self.can_address}) 10 12 0 0 0 0 0 0"
